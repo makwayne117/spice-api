1487f5fcef53eda74e237486c718789cbff7720c
sama9843 <sama9843@colorado.edu>
2023-04-04 15:08:08 -0600

    Orbit Test4

--- positioningRefactored.py
+++ positioningRefactored.py
@@ -32,6 +32,7 @@ def return_position():
     #utctim = "2004 jun 11 19:32:00"
     print(target)
     spiceypy.furnsh(METAKR)
+    utctim = datetime.datetime.now()
     temp = np.array(utctim)
     for i in range(29):
         temp.append(utctim-datetime.timedelta(30))
