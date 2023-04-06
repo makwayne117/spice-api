32757ae240e866316cbc28e7039f48c16648fcbe
sama9843 <sama9843@colorado.edu>
2023-04-06 15:59:04 -0600

    Testing Deploy

--- positioningRefactored.py
+++ positioningRefactored.py
@@ -38,7 +38,7 @@ def return_body_position():
 
     [return_pos, ltime] = spiceypy.spkpos(target, et, 'J2000',
                                           'LT+S', obs, )
-    #moved
+                                          
     spiceypy.unload(METAKR)
 
     print(jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]}))
