072f5eb57af9e435747575d4eefd1e36ba13d695
sama9843 <sama9843@colorado.edu>
2023-04-06 15:41:34 -0600

    New Route Bug Fix2

--- positioningRefactored.py
+++ positioningRefactored.py
@@ -35,12 +35,12 @@ def return_body_position():
     spiceypy.furnsh(METAKR)
 
     et = spiceypy.str2et(utctim)
-    
-    spiceypy.unload(METAKR)
 
     [return_pos, ltime] = spiceypy.spkpos(target, et, 'J2000',
                                           'LT+S', obs, )
 
+    spiceypy.unload(METAKR)
+    
     print(jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]}))
     
     return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]})
