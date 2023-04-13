bc0ae059a46a1ade2b235b70627b2e86ac963a42
KevinFLoaiza <kelo2418@colorado.edu>
2023-04-13 13:08:20 -0600

    mission test

--- positioningRefactored.py
+++ positioningRefactored.py
@@ -65,6 +65,27 @@ def return_body_position():
     
     return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]})
 
+@app.route('/mission', methods=['GET'])
+def return_spacecraft_position(mission):
+   # read missions dictionary from the file
+    with open('missions.json', 'r') as f:
+        missions = json.load(f)
+
+    kernel_urls = missions[mission]
+
+    #Lets add the leapsecond file to the kernel pool
+    kernel_urls.append("../kernels/leap.tls")
+    #Add all kernels to spice and compute data for Voyager 1
+    spiceypy.furnsh(kernel_urls)
+    target = request.args.get('mission')
+    utctime = request.args.get('utc')
+    obs = "SUN"
+    et = spiceypy.stret(utctime)
+    [return_pos, ltime] = spiceypy.spkpos(target, et, 'J2000', 'LT+S', obs)
+    spiceypy.unload(kernel_urls)
+    print(jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]}))
+    return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]})
+
 
 @app.route('/orbits', methods=['GET'])
 def return_position():
