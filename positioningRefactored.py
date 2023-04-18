34d8a63447ab8583cbee55daaf49dcd707a989a5
alberttmy1 <alta2479@colorado.edu>
2023-04-18 16:13:58 -0600

    fix

--- positioningRefactored.py
+++ positioningRefactored.py
@@ -66,11 +66,12 @@ def return_body_position():
     return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]})
 
 @app.route('/mission', methods=['GET'])
-def return_spacecraft_position(mission):
+def return_spacecraft_position():
    # read missions dictionary from the file
     with open('missions.json', 'r') as f:
         missions = json.load(f)
         print("in missions")
+    mission = request.args.get('mission')
     kernel_urls = missions[mission]
 
     #Lets add the leapsecond file to the kernel pool
