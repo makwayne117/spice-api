8b9d6f961a6fe2e9988c06ac25427ae859994b45
sama9843 <sama9843@colorado.edu>
2023-04-20 14:35:01 -0600

    Test New Routes

--- positioningRefactored.py
+++ positioningRefactored.py
@@ -68,8 +68,35 @@ def return_body_position():
     
     return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2], "r": radii[0]})
 
-@app.route('/mission', methods=['GET'])
-def return_spacecraft_position():
+@app.route('/mission_pos', methods=['GET'])
+def return_spacecraft_pos():
+   # read missions dictionary from the file
+   #
+    print("I'm here")
+    with open('missions.json', 'r') as f:
+        missions = json.load(f)
+        print("in missions")
+    mission = request.args.get('mission')
+    kernel_urls = missions[mission]
+    kernel_urls = ["kernels/"+mission +"/"+ x for x in kernel_urls]
+    print(kernel_urls)
+
+    #Lets add the leapsecond file to the kernel pool
+    kernel_urls.append("kernels/leap.tls")
+    #Add all kernels to spice and compute data for VOYAGER 1 1
+    spiceypy.furnsh(kernel_urls)
+    target = request.args.get('mission')
+    utctim = request.args.get('utc')
+    length = request.args.get('length')
+    obs = "SUN"
+    et = spiceypy.str2et(utctime)
+    [return_pos, ltime] = spiceypy.spkezr(target, et, 'J2000', 'LT+S', obs)
+    spiceypy.unload(kernel_urls)
+    print(jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2], "vx": return_pos[3], "vy": return_pos[4], "vz": return_pos[5]}))
+    return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]})
+
+@app.route('/mission_orbits', methods=['GET'])
+def return_spacecraft_orbit():
    # read missions dictionary from the file
    #
     print("I'm here")
